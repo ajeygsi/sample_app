2dd640ddb173926f215901eee55910def698c327
ajey <ajey@chronus.com>
2011-05-11 18:27:02 +0530

    finished pagination

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,8 +13,8 @@ class SessionsController < ApplicationController
       render 'new'
     else
       sign_in user
-      redirect_to user
-      # Sign the user in and redirect to the user's show page.
+      redirect_back_or user
+      
     end
   end
 
