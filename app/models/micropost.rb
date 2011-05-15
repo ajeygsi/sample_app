559ccee4cccb7b5512a79f68ea0dc4f41e9741ca
ajey <ajeygsi2@gmail.com>
2011-05-15 22:35:00 +0530

    will now try to add graffiti

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@ class Micropost < ActiveRecord::Base
   attr_accessible :content
 
   belongs_to :user
-  has_many :comments, :as => :commentable
+  has_many :comments, :as => :commentable, :dependent => :destroy
 
   validates :content, :presence => true, :length => { :maximum => 140 }
   validates :user_id, :presence => true
